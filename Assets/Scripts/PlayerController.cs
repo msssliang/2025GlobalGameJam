6d69dd84286110b1c0bf23e68781ded3fc8c8332
EffieLiang <EffieLiang@icecone.net>
2025-01-25 14:30:28 +0800

    功能：玩家移動改為按住移動

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,8 @@ public class PlayerController : MonoBehaviour
     [field: SerializeField]
     public float moveAmount { get; private set; }
     [field: SerializeField]
+    public float PressTime { get; private set; } = 0.1f;
+    [field: SerializeField]
     public Vector3 forward { get; private set; }
     [field: SerializeField]
     public Vector3 right { get; private set; }
@@ -32,49 +34,67 @@ public class PlayerController : MonoBehaviour
     }
     void player1Control()
     {
-        if (Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKey(KeyCode.W))
         {
             Move(forward);
         }
-        if (Input.GetKeyDown(KeyCode.S))
+        if (Input.GetKey(KeyCode.S))
         {
             Move(back);
         }
-        if (Input.GetKeyDown(KeyCode.A))
+        if (Input.GetKey(KeyCode.A))
         {
             Move(left);
         }
-        if (Input.GetKeyDown(KeyCode.D))
+        if (Input.GetKey(KeyCode.D))
         {
             Move(right);
+        }
+        if (Input.GetKeyUp(KeyCode.W) ||
+            Input.GetKeyUp(KeyCode.S) ||
+            Input.GetKeyUp(KeyCode.A) ||
+            Input.GetKeyUp(KeyCode.D))
+        {
+            pressTime = 0;
         }
     }
     void player2Control()
     {
-        if (Input.GetKeyDown(KeyCode.I))
+        if (Input.GetKey(KeyCode.I))
         {
             Move(forward);
         }
-        if (Input.GetKeyDown(KeyCode.K))
+        if (Input.GetKey(KeyCode.K))
         {
             Move(back);
         }
-        if (Input.GetKeyDown(KeyCode.J))
+        if (Input.GetKey(KeyCode.J))
         {
             Move(left);
         }
-        if (Input.GetKeyDown(KeyCode.L))
+        if (Input.GetKey(KeyCode.L))
         {
             Move(right);
         }
+        if (Input.GetKeyUp(KeyCode.I) ||
+            Input.GetKeyUp(KeyCode.K) ||
+            Input.GetKeyUp(KeyCode.J) ||
+            Input.GetKeyUp(KeyCode.L))
+        {
+            pressTime = 0;
+        }
     }
+    float pressTime = 0;
     void Move(Vector3 direction)
     {
         GameManager gameManager = FindFirstObjectByType<GameManager>();
         Vector3 moveTarget = direction * moveAmount + transform.position;
         Vector2 checkPosition = new Vector2(moveTarget.x, moveTarget.z);
         if (gameManager.CheckMovable(checkPosition) == false) return;
+        pressTime += Time.deltaTime;
+        if (pressTime < PressTime) return;
         transform.position += direction * moveAmount;
         OnMove?.Invoke(new Vector2(transform.position.x, transform.position.z));
+        pressTime = 0;
     }
 }
